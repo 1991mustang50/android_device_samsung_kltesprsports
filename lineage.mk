892fda927f8c3fa7f08b8ef6c306a92d3c160556
evilarceus <evilarceus@gmail.com>
2017-01-26 16:48:46 -0800

    Change cm to lineage

--- lineage.mk
+++ lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kltesprsports/full_kltesprsports.mk)
 
 PRODUCT_DEVICE := kltesprsports
-PRODUCT_NAME := cm_kltesprsports
+PRODUCT_NAME := lineage_kltesprsports
